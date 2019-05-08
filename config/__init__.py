528059e0e9a6208c1d0ced4cf35089c1eb67d5b0
Khanh Nguyen <nguyenkhanh97nd@gmail.com>
2019-05-09 00:25:42 +0700

    first commit

--- config/__init__.py
+++ config/__init__.py
@@ -8,4 +8,4 @@ API_SETUP_LOCAL = "https://graph.facebook.com/v2.6/me/messenger_profile"
 
 # TOKEN #
 VERIFY_TOKEN = "random_string_verify"
-PAGE_ACCESS_TOKEN = "EAAFZCfGSv5cgBACqwjHV9C3ZAHd3ZCsGNox5hXTbatl5q6HyNdBLONZBgC3l3DCC61ovbn3GwZBSUXdbiwQh3knZAk3mLKqZBOZALLC81cYNlxG38ZCO6aDOneZB7oiqerSf31iouPXnyM1iD55FngbdZAGIRTmnUGZAtrigZBFXzFhsTAQZDZD"
+PAGE_ACCESS_TOKEN = "your_access_token"
